c64f2d94cc496875f63d9e301744d526e2bf551a
Ryan Hughes <ryan.hughes@attx.tech>
2025-05-20 09:18:27 -0600

    add button to hello world app

--- hello_world.main.cpp
+++ hello_world.main.cpp
@@ -8,9 +8,15 @@ int main(int , char *[])
     auto guiFunction = []() {
         ImGui::Text("Hello, ");                    // Display a simple label
         HelloImGui::ImageFromAsset("world.jpg");   // Display a static image
-        if (ImGui::Button("Bye!"))                 // Display a button
+        static bool show_goodbye_text = false;
+        if (ImGui::Button("Bye!")) {               // Display a button
             // and immediately handle its action if it is clicked!
-            HelloImGui::GetRunnerParams()->appShallExit = true;
+            // HelloImGui::GetRunnerParams()->appShallExit = true;
+            show_goodbye_text = true;
+        }
+        if (show_goodbye_text) {
+            ImGui::Text("Just kidding, you can never leave :)");
+        }
      };
     HelloImGui::Run(guiFunction, "Hello, globe", true);
     return 0;
